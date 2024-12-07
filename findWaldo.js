5148200e82d07e251f8fe4cf8298e86fef780df7
Anika Nawar <nawarani47@gmail.com>
2024-12-07 15:13:06 -0500

    findWaldo forEach version

--- findWaldo.js
+++ findWaldo.js
@@ -1,15 +1,14 @@
 // The second argument/parameter is expected to be a (callback) function
-const findWaldo = function (names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
-    if (name === "Waldo") {
-      found(i); // execute callback
-    }
-  }
+const findWaldo = function(names, check) {
+  names.forEach((element, index) => {
+    check(element, index);
+  });
 };
 
-const actionWhenFound = function (index) {
-  console.log(`Found Waldo at index ${index}!`);
+const checkWaldo = (name, index) => {
+  if (name === 'Waldo') {
+    console.log(`Found Waldo at index ${index}!`);
+  }
 };
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
